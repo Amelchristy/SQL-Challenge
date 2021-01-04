c34e94e64e955e8ca40ab4db7c3035302c967dfb
Amelchristy <Amel_christy@users.noreply.github.com>
2021-01-04 03:37:44 -0500

    Final Push

--- EmployeeSQL/Employees_db - Queries.sql
+++ EmployeeSQL/Employees_db - Queries.sql
@@ -39,12 +39,14 @@ FROM employees
 	WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 	
 --6
-SELECT
-first_name, 
-last_name, 
-sex 
-FROM employees
-	WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales';
+
 	
 --7
 SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
